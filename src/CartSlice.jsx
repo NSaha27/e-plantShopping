40b35049e2b0209b8674de55a13e5d316543f4e4
Niladri Saha <sahaniladri27y@gmail.com>
2026-08-19 11:00:54 -0400

    update CartSlice by removing unnecessary code snippet

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -11,32 +11,18 @@ export const CartSlice = createSlice({
       const existingItem = state.items.find((item) => item.name === name);
       if (existingItem) {
         existingItem.quantity++;
-        state.items.map(item => {
-          if(item.name === existingItem.name){
-            return existingItem;
-          }else{
-            return item;
-          }
-        })
       } else {
         state.items.push({ name, image, cost, quantity: 1 });
       }
     },
     removeItem: (state, action) => {
-      state.items.filter((item) => item.name !== action.payload);
+      state.items = state.items.filter((item) => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
       const { name, quantity } = action.payload;
       const itemToUpdate = state.items.find((item) => item.name === name);
       if (itemToUpdate) {
         itemToUpdate.quantity = quantity;
-        state.items.map(item => {
-          if(item.name === name){
-            return itemToUpdate;
-          }else{
-            return item;
-          }
-        })
       }
     },
   },
